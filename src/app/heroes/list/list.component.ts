39df379777494f7b333d03f362f45ed003c3731b
HugoYo <hugohbuenrostro@gmail.com>
2023-04-25 09:42:20 -0600

    feat: ngFor

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames: string[] = [
+    'Spiderman',
+    'Hulk',
+    'Ironman',
+    'Thor',
+    'Hawkeye'
+  ];
+  public deletedHeroes: string[] = [];
+
+  deleteLastHeroe() {
+    if(this.heroNames.length !== 0) {
+      const lastHeroe = this.heroNames.pop()!;
+      this.deletedHeroes.push(lastHeroe);
+    } else {
+      console.log('todo fue eliminado');
+    }
+  }
+  
 }
